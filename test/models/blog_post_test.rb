0925f1ab9534e53d90f2afe931447cea84fcd06a
KaiKidThief <roxas.269269@meycauayan.sti.edu.ph>
2024-06-28 07:28:45 +0800

    Add tests for blog posts

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blog post" do
+    binding.irb
+    assert BlogPost.new(published_at: nil).draft?
+  end
+
+  test "draft? returns false for published blog post" do
+      refute BlogPost.new(published_at: 1.year.ago).draft?
+  end
+
+  test "draft? returns false for scheduled blog post" do
+    refute BlogPost.new(published_at: 1.year.from_now).draft?
+  end
 end
